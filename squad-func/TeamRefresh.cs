ac655386d09ad96c853068e46088bc31d107874e
seanr89 <srafferty89@gmail.com>
2026-05-15 15:41:54 +0100

    chore: update TeamRefresh timer trigger schedule to 15:00-19:00 UTC

--- squad-func/TeamRefresh.cs
+++ squad-func/TeamRefresh.cs
@@ -24,7 +24,7 @@ IApiService apiService, DatabaseService databaseService)
     /// </summary>
     /// <param name="myTimer">The timer trigger info.</param>
     [Function("TeamRefresh")]
-    public async Task Run([TimerTrigger("0 0 5-12 * * *")] TimerInfo myTimer)
+    public async Task Run([TimerTrigger("0 0 15-19 * * *")] TimerInfo myTimer)
     {
         // re-working logic flow here to check via fixture date is not null
         var incompleteFixtures = await _context.Fixtures
