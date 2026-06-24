862b09d16645195cdbba18ecce02dfe5d6ca34e4
seanr89 <srafferty89@gmail.com>
2026-06-24 20:46:01 +0100

    efcore twaeks

--- squad-domain/Models/Feedback.cs
+++ squad-domain/Models/Feedback.cs
@@ -1,10 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace squad_domain.Models;
 
+[Table("feedback")]
 public class Feedback
 {
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
+    [Column("name")]
     public string Name { get; set; } = null!;
+    [Column("email")]
     public string Email { get; set; } = null!;
+    [Column("message")]
     public string Message { get; set; } = null!;
+    [Column("created_at")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
